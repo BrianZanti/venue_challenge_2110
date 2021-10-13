68f5fcba495bb66e043d07c984ca18703d0db377
Jacob Jenner <jennerjacob@gmail.com>
2021-10-13 11:40:48 -0600

    Initial Commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,28 @@
 class Venue
+  def initialize(name, number)
+    @name = name
+    @number = number
+    @patrons = []
+
+def name
+  @name
+end
+
+def number
+  @number
+end
+
+def add_patron(patron)
+  @patrons << patron
+end
+
+def patrons
+  @patrons
+end
+
+def yell_at_patrons(patron)
+  @patrons << yell_at_patrons
+end
+
+  end
 end
