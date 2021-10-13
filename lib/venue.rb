37c441242e77cebc1fd86ba85e9f3ff29104b0ce
Robin Dittrich <robinodittrich@gmail.com>
2021-10-13 10:32:53 -0700

    Add venue and associated test file

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,20 @@
 class Venue
+  attr_reader :name, :capacity
+  def initialize (name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def patrons
+    patrons = []
+  end
+
+  def add_patron(new_patron)
+    patrons << new_patron.new
+  end
+
+  def yell_at_patrons
+    patrons.upcase
+  end
 end
