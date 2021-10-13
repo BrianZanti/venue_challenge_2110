d4fde9ab5e165e77845eda02a11c1d7d2473d175
Russell Rockwood <russell.rockwood@gmail.com>
2021-10-13 12:08:07 -0500

    Initialcommit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,30 @@
 class Venue
+  attr_accessor :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons << name
+  end
+
+  def yell_at_patrons
+    uppercased_names = []
+    @patrons.each do |name|
+      uppercased_names << name.upcase
+    end
+    uppercased_names
+  end
+
+  def over_capacity?
+    @patrons.length > @capacity ? true : false
+  end
+
+  def kick_out
+    @patrons.shift
+    @patrons
+  end
 end
