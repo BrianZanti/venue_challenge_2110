ed2638e6dfa569e640b1615915261a8af7170f78
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-13 11:31:39 -0600

    Add changes to indepedent challenge

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,38 @@
 class Venue
+
+  attr_reader :name, :capacity, :patrons #:yell_at_patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+    #@yell_at_patrons =[]
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map(&:upcase) #found on google when I couldn't get.each to work
+   # @yell_at_patrons = []
+   # patrons.each do |patron|
+   #   @yell_at_patrons << patron.upcase
+  end
+
+  def over_capacity?
+    if @patrons.count > 4
+      true
+    elsif @patrons.count <= 4
+      false
+    else
+      nil
+    end
+  end
+
+  def kick_out
+      while @patrons.count > 4
+        @patrons.pop
+      end
+    end
 end
