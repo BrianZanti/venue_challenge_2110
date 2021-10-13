1e5e21d3d5314de4afbaad454f2273aa692289a2
Joel Grant <joelmgrant@me.com>
2021-10-13 11:27:05 -0600

    Add Iteration/tests 2-4

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,7 +1,10 @@
 class Venue
+  attr_accessor :bird_type, :capacity, :capacity
+
   def initialize(bird_type, capacity)
     @bird = bird_type
     @capacity = capacity
+    @patrons = []
   end
 
   def name
@@ -13,7 +16,31 @@ class Venue
   end
 
   def patrons
-    patrons = []
+    @patrons
+  end
+
+  def add_patron(name)
+    @patrons << name
+  end
+
+  def yell_at_patrons
+    yelled_names = []
+    patrons.each do |patron|
+      yelled_names << patron.upcase
+    end
+    return yelled_names
+  end
+
+  def over_capacity?
+
+    return patrons.size > capacity
+
+  end
+
+  def kick_out
+    while over_capacity? == true
+      patrons.pop
+    end
   end
 
 
