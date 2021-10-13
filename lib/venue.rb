aa6fa5cb29a25d44567b61e5d4c1e0df860cd140
Tim Roeder <timothyjroeder@gmail.com>
2021-10-13 10:47:49 -0600

    Created methods for Venue class iteration 1-4 features

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,38 @@
 class Venue
+  attr_accessor :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron_name)
+    # Add addition patrons to the venue list.
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons
+    # Iterate through patrons and change string value to upcase.
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    # If the number of patrons is greater than capacity return true.
+    @patrons.count > @capacity ? true : false
+  end
+
+  def kick_out
+    if self.over_capacity? == true
+      # over_count is how many patrons are we over capacity.
+      over_count = @patrons.count - @capacity
+
+      # Remove the last entrants down to the capacity limit.
+      over_count.times do
+        @patrons.pop
+      end
+    end
+  end
 end
