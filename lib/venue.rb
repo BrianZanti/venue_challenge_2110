126d639a8c7914d6f18fa97de33ff4ba48680dab
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-13 10:18:24 -0600

    Add instance variables and methods

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,24 @@
 class Venue
+  attr_accessor :patrons
+  attr_reader :name, :capacity
+  def initialize(name, int)
+    @name = name
+    @capacity = int
+    @patrons = []
+  end
+
+  def add_patron(i)
+    self.patrons << i.to_s
+  end
+
+  def yell_at_patrons
+    modified_array = []
+
+    self.patrons.each do |i|
+      modified_array << i.upcase
+    end
+
+    return modified_array
+  end
+
 end
