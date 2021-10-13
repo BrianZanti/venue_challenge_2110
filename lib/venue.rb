ef56f5e51452c98e09e280b54f131977945a9e18
craig cardwell <wizeduvcc@gmail.com>
2021-10-13 10:50:10 -0600

    feat: add iterations one thru four with corresponding methods and passing tests

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,31 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons  = []
+  end
+
+  def add_patron(patron)
+    patrons << patron
+  end
+
+  def yell_at_patrons
+    yelled_at = []
+    @patrons.each do |patron|
+    yelled_at << patron.upcase
+    end
+    yelled_at
+  end
+
+  def over_capacity?
+    patrons.count > 4
+  end
+
+  def kick_out
+    patrons.pop
+    over_capacity?
+  end
 end
