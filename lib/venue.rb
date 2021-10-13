47279cadda6fbdde1339065a164c38e721642439
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-13 11:26:36 -0600

    Complete Iterations

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,30 @@
 class Venue
+  attr_reader :name, :capacity
+  attr_accessor :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+
+  end
+  def add_patron(patron)
+    @patrons << patron
+  end
+  def yell_at_patrons
+    @patrons.map{|patron| patron.upcase}
+  end
+  def over_capacity?
+    return true if @patrons.length > @capacity
+    false
+  end
+  def number_of_patrons
+    @patrons.length
+  end
+  def kick_out
+    if @patrons.length > @capacity
+      space_needed = @patrons.length - @capacity
+      @patrons.delete(space_needed)
+      @patrons
+    end
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,28 +1,28 @@
 require 'rspec'
 require './lib/venue'
 
-describe Venue do
+RSpec.describe Venue do
   describe '#initialize' do
     it 'is a venue' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,7 +32,7 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -44,12 +44,40 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
+    end
+  end
+  describe '#over_capacity' do
+    it 'returns true if more patrons than capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      venue.over_capacity?
+      expect(venue.over_capacity?).to eq false
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      venue.over_capacity?
+      expect(venue.over_capacity?).to eq true
+    end
+  end
+  describe '#kick_out' do
+    it 'removes patrons to become at capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      venue.kick_out
+      expect(venue.number_of_patrons).to eq 4
+      expect(venue.over_capacity?).to eq false
+
     end
   end
 end
