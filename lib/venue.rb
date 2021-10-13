b2c3f2341d85af1e484f312ac4dc2547d3753873
Jessica Grazulis <jlgrazulis@gmail.com>
2021-10-13 11:33:50 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,30 @@
 class Venue
+
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity, patrons = [])
+    @name = name
+    @capacity = capacity
+    @patrons = patrons
+  end
+
+  def add_patron()
+    @patrons['name']
+  end
+
+  def yell_at_patrons
+    @patrons.upcase
+  end
 end
+#
+# pry(main)> venue = Venue.new('Bluebird', 4)
+# => #<Venue:0x00007fed7c11dbd8 @capacity=4, @name="Bluebird", @patrons=[]>
+#
+# pry(main)> venue.name
+# => "Bluebird"
+#
+# pry(main)> venue.capacity
+# => 4
+#
+# pry(main)> venue.patrons
+# => []
