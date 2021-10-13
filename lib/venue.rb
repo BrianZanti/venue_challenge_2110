d24f3c9b32699865796e0a51b295c2896e5066fe
Sam Iavri <sammi80134@gmail.com>
2021-10-13 11:28:16 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,19 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize (venue, capacity)
+    @name = venue
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patrons)
+    @patrons = ['Mike', 'Megan', 'Bob']
+
+  end
+
+  def yell_at_patrons(patrons)
+    @patrons = ['Mike', 'Megan', 'Bob'].upcase
+  end
+
 end
