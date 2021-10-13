d5ddc522dde332fba9d6ace2fa7e99bef009d37f
Eric Chon <echon92@gmail.com>
2021-10-13 11:24:40 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,24 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(add)
+     @patrons.push add
+
+  end
+
+  def yell_at_patrons
+    @patrons.each do |patron|
+      @patrons.upcase << patron
+    end
+
+  end
+
 end
