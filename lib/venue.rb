d373201810e310495e9972741017251c83000091
Wade Naughton <wadenaughton@gmail.com>
2021-10-13 13:31:42 -0400

    initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,19 @@
 class Venue
+  attr_reader :name, :capacity
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def patrons
+    []
+  end
+
+  def add_patron(add_patron)
+    @patrons = add_patron
+
+  end
+
 end
