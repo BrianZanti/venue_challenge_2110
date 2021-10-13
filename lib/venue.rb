113109a2cd3e8a89d855a0c6ebb279f47b87af4b
Chris Hewitt <agop5134@gmail.com>
2021-10-13 10:34:20 -0600

    Initial and final commit :)

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,34 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    if @patrons.count > capacity
+      true
+    else
+      false
+    end
+  end
+
+  def kick_out
+    if self.over_capacity? == true
+      @patrons.shift until self.over_capacity? == false
+    end
+  end
 end
