92eb389cecb8c3b7fc90d646f7078cc35b761cfc
Brad Breiten <jonathan@breiten.us>
2021-10-13 10:28:19 -0600

    Complete iteration 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+
+  attr_accessor :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron_name)
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons
+    yelled_patrons = []
+    yelled_patrons << @patrons.map(&:upcase)
+    return yelled_patrons.flatten
+  end
+
 end
