f0b5299b67deabd7f26a5da110daa0bf8b9cd364
Joel Grant <joelmgrant@me.com>
2021-10-13 10:25:36 -0600

    Add iteration 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,20 @@
 class Venue
+  def initialize(bird_type, capacity)
+    @bird = bird_type
+    @capacity = capacity
+  end
+
+  def name
+    @bird
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def patrons
+    patrons = []
+  end
+
+
 end
