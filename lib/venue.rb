bb36b54244d81fcf948ab3efb134f9ca64294b00
Kelly Anderson <kean9528@colorado.edu>
2021-10-13 11:32:20 -0600

    do challenge

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,25 @@
 class Venue
-end
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity, patrons = [])
+    @name = name
+    @capacity = capacity
+    @patrons = patrons
+  end
+  def add_patron(patron)
+    @patrons += [patron]
+  end
+  def yell_at_patrons
+    @patrons.each do |patron|
+      patron.upcase
+      @patrons << patron
+    end
+  end
+  def over_capacity?
+    if @patrons >= 4
+      return true
+    else
+      return false
+    end
+  end
+
+  end
