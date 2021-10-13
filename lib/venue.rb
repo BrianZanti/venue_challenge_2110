84704c508de11d137881fc6e43fc6b1fc0120eae
Dylan Harper <dylanharper4@icloud.com>
2021-10-13 10:24:23 -0700

    Add independent challenge

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,38 @@
 class Venue
+
+  attr_reader :name, :capacity
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def prints_venue_name
+    puts @name
+  end
+
+  def prints_capacity
+    puts @capacity
+  end
+
+  def patrons
+    @patrons
+  end
+
+  def add_patron(first_name)
+    @patrons << first_name
+  end
+
+  def yell_at_patrons
+    @patrons.map(&:upcase)
+  end
+
+  def over_capacity # error message
+    if @patrons.count >= @capacity
+      puts true
+    else
+      puts false
+    end
+  end
 end
