f8cbd56364c6f935ed0f1684d937924a66cf87d9
Mallory Vining <mallorygallery12@gmail.com>
2021-10-13 10:41:17 -0600

    Finish IC

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,28 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(new_patrons)
+    @patrons << new_patrons
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+       patron.upcase
+    end
+  end
+
+  def over_capacity?
+     @patrons.length > @capacity
+  end
+
+  def kick_out
+    @patrons.shift
+  end
 end
