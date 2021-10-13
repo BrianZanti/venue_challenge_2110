69567cbe64103806fa6ff20de881a6d15b81e351
Paul Leonard <paulpaulleonard@gmail.com>
2021-10-13 12:31:44 -0400

    Final commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,36 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons  = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    upcased_patrons = patrons.map do |patron|
+      patron.upcase
+    end
+    upcased_patrons
+  end
+
+  def over_capacity?
+    if @patrons.length <= @capacity
+      false
+    elsif @patrons.length > @capacity
+      true
+    end
+  end
+
+  def kick_out
+    if @patrons.length > @capacity
+      @patrons.shift
+    end
+  end
 end
