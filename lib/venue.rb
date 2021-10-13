0b2a496b1797640957c2ac60e58cfa7129d184c9
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-10-13 11:12:03 -0600

    complete challenge

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,42 @@
 class Venue
+
+  attr_reader :name, :capacity, :patrons
+
+  def initialize (name_arg, capacity_arg)
+    @name = name_arg
+    @capacity = capacity_arg
+    @patrons = []
+  end
+
+  def add_patron (name)
+    patrons.push(name)
+  end
+
+  def yell_at_patrons
+    capitilized_names = []
+    patrons.each do |patron_name|
+      capitilized_names.push(patron_name.upcase)
+    end
+    return capitilized_names
+  end
+
+  def over_capacity?
+    if patrons.count > capacity
+      over_capacity = true
+    else
+      over_capacity = false
+    end
+    return over_capacity
+  end
+
+  def kick_out
+    if self.over_capacity?
+      while patrons.count > capacity
+        patrons.pop
+      end
+    end
+    return patrons.count
+  end
+
+
 end
